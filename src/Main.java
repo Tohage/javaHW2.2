a488b77fb6dd64cb8c5a12b6a86bd1c043bcc576
Tohage89 <avgorin89@gmail.com>
2025-02-25 20:11:25 +0300

    addCodeBonus

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Расчет колличества бонусных рублей");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int onTheClientAccount = 300;
+        int replenishmentAccount = 1100;
+        int totalAmountAccount = onTheClientAccount + replenishmentAccount;
+
+        System.out.println("На счету у клиента - " + onTheClientAccount + " p");
+        System.out.println("Пополнение счета на - " + replenishmentAccount + " p");
+
+        if (replenishmentAccount >= 1000) {
+            int bonus = replenishmentAccount / 100;
+
+            System.out.println("Итого на счету - " + (totalAmountAccount + bonus) + " p");
+            System.out.println("Начисленные бонусы - " + bonus + " p");
+        } else {
+            System.out.println("Итого на счету - " + totalAmountAccount + " p");
+            System.out.println("Начисленные бонусы - 0 p");
+
         }
     }
 }
