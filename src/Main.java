6c58cfbda716e87c35c70792c499ea21eb50d131
Tohage89 <avgorin89@gmail.com>
2025-02-26 10:20:31 +0300

    changeCodeBonus

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
         System.out.println("Расчет колличества бонусных рублей");
 
         int onTheClientAccount = 300;
-        int replenishmentAccount = 1100;
+        int replenishmentAccount = 1211;
         int totalAmountAccount = onTheClientAccount + replenishmentAccount;
 
         System.out.println("На счету у клиента - " + onTheClientAccount + " p");
         System.out.println("Пополнение счета на - " + replenishmentAccount + " p");
 
-        if (replenishmentAccount >= 1000) {
+        if (replenishmentAccount >= 1100) {
             int bonus = replenishmentAccount / 100;
 
             System.out.println("Итого на счету - " + (totalAmountAccount + bonus) + " p");
@@ -19,5 +19,6 @@ public class Main {
             System.out.println("Начисленные бонусы - 0 p");
 
         }
+
     }
 }
